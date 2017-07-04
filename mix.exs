f8e02dee0c332f0dcf03662f9a3b37b371caadf6
Andy <flashgame73@gmail.com>
2017-07-04 18:24:18 +0400

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SocketioEmitter.Mixfile do
 
   def project do
     [app: :socketio_emitter,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
