4e36f65ae82dd1333d758bf146f2441c1e359f37
Andrey <chugunov@users.noreply.github.com>
2017-07-04 14:42:04 +0400

    Update mix.exs
    Downgrade Elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SocketioEmitter.Mixfile do
   def project do
     [app: :socketio_emitter,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.2",
      description: description(),
      package: package(),
      build_embedded: Mix.env == :prod,
